d2bdd09f1a56bcc3e3eedb02916b0a1f5f4b50f5
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2022-06-23 10:43:06 +0530

    Some page priority changes

--- next-sitemap.js
+++ next-sitemap.js
@@ -12,25 +12,7 @@ module.exports = {
                 alternateRefs: config.alternateRefs
             }
         }
-        if(path == '/bangalore/car-transport'){
-            return {
-                loc: path,
-                changefreq: config.changefreq,
-                priority: 0.9,
-                lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-                alternateRefs: config.alternateRefs
-            }
-        }
-        if(path == '/delhi/car-transport'){
-            return {
-                loc: path,
-                changefreq: config.changefreq,
-                priority: 0.9,
-                lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
-                alternateRefs: config.alternateRefs
-            }
-        }
-        if(path == '/bangalore/car-transport'){
+        if(path.includes('bangalore') || path.includes('delhi')){
             return {
                 loc: path,
                 changefreq: config.changefreq,
